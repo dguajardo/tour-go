eafad790d7f922347ef538f412cb4775f948ddba
DGT <diego.guajardo@hotmail.cl>
2024-04-19 01:03:38 -0400

    if-else

--- hello.go
+++ hello.go
@@ -61,7 +61,10 @@ func sqrt(x float64) string {
 func pow(x, n, lim float64) float64 {
 	if v := math.Pow(x, n); v < lim {
 		return v
+	} else {
+		fmt.Printf("%g >= %g\n", v, lim)
 	}
+	// can't use v here, though
 	return lim
 }
 
