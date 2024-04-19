f7b1e88f13f13c31ed15f57859c6d8f8d664e0ff
DGT <diego.guajardo@hotmail.cl>
2024-04-19 01:16:55 -0400

    switch eval order

--- hello.go
+++ hello.go
@@ -178,4 +178,18 @@ func main() {
 		fmt.Printf("%s.\n", os)
 	}
 
+	// switch eval order
+	fmt.Println("When's Saturday?")
+	today := time.Now().Weekday()
+	switch time.Saturday {
+	case today + 0:
+		fmt.Println("Today.")
+	case today + 1:
+		fmt.Println("Tomorrow.")
+	case today + 2:
+		fmt.Println("In two days.")
+	default:
+		fmt.Println("Too far away.")
+	}
+
 }
