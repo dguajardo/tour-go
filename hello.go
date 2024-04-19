0455d51b5817262f9306547f064364709abcf0a3
DGT <diego.guajardo@hotmail.cl>
2024-04-19 00:44:11 -0400

    type inference

--- hello.go
+++ hello.go
@@ -13,13 +13,12 @@ import (
 // Variables with initializers
 //var i, j int = 1, 2
 
-//Basic types outer
+// Basic types outer
 var (
 	ToBe   bool       = false
 	MaxInt uint64     = 1<<64 - 1
 	z      complex128 = cmplx.Sqrt(-5 + 12i)
 )
-
 
 func add(x int, y int) int {
 	return x + y
@@ -49,7 +48,7 @@ func main() {
 	// fmt.Println(i, c, python, java)
 	// var c, python, java = true, false, "no!"
 	// fmt.Println(i, j, c, python, java)
-	
+
 	//Short variable declarations
 	//var i, j int = 1, 2
 	//k := 3
@@ -60,10 +59,20 @@ func main() {
 	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
 	fmt.Printf("Type: %T Value: %v\n", z, z)
 
-	// Zero values
-	var i int
-	var f float64
-	var b bool
-	var s string
-	fmt.Printf("%v %v %v %q\n", i, f, b, s)
+	// // Zero values
+	// var i int
+	// //var f float64
+	// var b bool
+	// var s string
+	// //fmt.Printf("%v %v %v %q\n", i, f, b, s)
+
+	//Type conversions
+	var x, y int = 3, 4
+	var f float64 = math.Sqrt(float64(x*x + y*y))
+	var z uint = uint(f)
+	fmt.Println(x, y, z)
+
+	// Type inference
+	v := 42 // change me!
+	fmt.Printf("v is of type %T\n", v)
 }
