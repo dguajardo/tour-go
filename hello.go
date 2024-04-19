c684da92d29b1cc162af79752b41b5d2a075706d
DGT <diego.guajardo@hotmail.cl>
2024-04-19 00:50:25 -0400

    numeric constants

--- hello.go
+++ hello.go
@@ -22,6 +22,14 @@ var (
 
 const Pi = 3.14
 
+const (
+	// Create a huge number by shifting a 1 bit left 100 places.
+	// In other words, the binary number that is 1 followed by 100 zeroes.
+	Big = 1 << 100
+	// Shift it right again 99 places, so we end up with 1<<1, or 2.
+	Small = Big >> 99
+)
+
 func add(x int, y int) int {
 	return x + y
 }
@@ -34,6 +42,11 @@ func split(sum int) (x, y int) {
 	x = sum * 4 / 9
 	y = sum - x
 	return
+}
+
+func needInt(x int) int { return x*10 + 1 }
+func needFloat(x float64) float64 {
+	return x * 0.1
 }
 
 func main() {
@@ -85,4 +98,10 @@ func main() {
 
 	const Truth = true
 	fmt.Println("Go rules?", Truth)
+
+	// Numeric Constants
+	fmt.Println(needInt(Small))
+	fmt.Println(needFloat(Small))
+	fmt.Println(needFloat(Big))
+
 }
