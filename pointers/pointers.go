f681399eeaebcd9f505f8692f3cd1225df5b7295
DGT <diego.guajardo@hotmail.cl>
2024-04-23 11:32:03 -0400

    ft: pointer to struct

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -21,4 +21,13 @@ func main() {
 
 	// Structs
 	fmt.Println(Vertex{1, 2})
+
+	v := Vertex{1, 2}
+	v.X = 4
+	fmt.Println(v.X)
+
+	// pointers to structs
+	point := &v
+	point.X = 1e9
+	fmt.Println(v)
 }
