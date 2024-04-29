5684ccd4e6b2760251e5e668460f55805fb564bf
DGT <diego.guajardo@hotmail.cl>
2024-04-28 22:17:46 -0400

    feat: Arrays

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -40,4 +40,68 @@ func main() {
 
 	// struct literal
 	fmt.Println(v1, p, v2, v3)
+
+	//Arrays
+	var a [2]string
+	a[0] = "Hello"
+	a[1] = "World"
+	fmt.Println(a[0], a[1])
+	fmt.Println(a)
+
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+	fmt.Println(primes)
+
+	// Array has fixed size so can be dynamically sized with slice
+
+	var s []int = primes[1:4]
+	fmt.Println(s)
+
+	// slice are like references to arrays
+
+	names := [4]string{
+		"John",
+		"Paul",
+		"George",
+		"Ringo",
+	}
+	fmt.Println(names)
+
+	aa := names[0:2]
+	b := names[1:3]
+	fmt.Println(aa, b)
+
+	b[0] = "XXX"
+	fmt.Println(aa, b)
+	fmt.Println(names)
+
+	//Slice literals
+	q := []int{2, 3, 5, 7, 11, 13}
+	fmt.Println(q)
+
+	r := []bool{true, false, true, true, false, true}
+	fmt.Println(r)
+
+	sy := []struct {
+		i int
+		b bool
+	}{
+		{2, true},
+		{3, false},
+		{5, true},
+		{7, true},
+		{11, false},
+		{13, true},
+	}
+	fmt.Println(sy)
+
+	//slice defaults
+	q = q[1:4]
+	fmt.Println(q)
+
+	q = q[:2]
+	fmt.Println(q)
+
+	q = q[1:]
+	fmt.Println(q)
+
 }
